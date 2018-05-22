9acc07b354eead9621546c3a65fcbb02e028e9ae
B-eck37 <brent.eckert7@gmail.com>
2018-05-22 00:13:35 -0700

    Minor updates - contact wip

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,18 @@ class Contact extends Component {
         return (
             <div>
                 <Header />
+                <div className="container">
                 <h1>Contact Component</h1>
+                <form action="">
+                    <input name='name'type="text"/>
+                    <input name='email' type="text"/>
+                    <textarea name="content" id="" cols="30" rows="10"></textarea>
+                    <div>
+                        <button>Send</button>
+                        <button>Clear</button>
+                    </div>
+                </form>
+                </div>
             </div>
         )
     }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import './CSS/components.css'
+import GoMarkGithub from 'react-icons/lib/go/mark-github'
 
 class Projects extends Component {
     constructor(props) {
@@ -15,11 +16,12 @@ class Projects extends Component {
                 <Header />
                 <div className="container">
                 
-                    <h1>Projects component</h1>
+                    {/* <h1>Projects component</h1> */}
 
                     <div className='project-display'>   
                         <div className="project one"></div>
                         <div className="project two"></div>
+                        <GoMarkGithub size={100} style={{margin: '0px auto', color: 'rgb(125, 0, 170)'}}/>
                         <div className="project three"></div>
                         <div className="project four"></div>
                     </div>
