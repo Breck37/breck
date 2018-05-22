66861acc66d5aee3232c6fc7598a0caa3eb491e5
B-eck37 <brent.eckert7@gmail.com>
2018-05-21 22:10:01 -0700

    Grid WIP

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import './CSS/components.css'
 
 class Projects extends Component {
     constructor(props) {
@@ -13,7 +14,15 @@ class Projects extends Component {
             <div>
                 <Header />
                 <div className="container">
-                <h1>Projects component</h1>
+                
+                    <h1>Projects component</h1>
+
+                    <div className='project-display'>   
+                        <div className="project one"></div>
+                        <div className="project two"></div>
+                        <div className="project three"></div>
+                        <div className="project four"></div>
+                    </div>
                 </div>
             </div>
         )
